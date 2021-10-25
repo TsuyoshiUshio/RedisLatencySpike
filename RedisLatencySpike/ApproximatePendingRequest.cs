072586b8e5e4ee8fd4e9e84133c1e2672a74fc3f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2021-10-25 09:58:57 -0700

    Add project files.

---
+++ RedisLatencySpike/ApproximatePendingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisLatencySpike
+{
+    public class ApproximatePendingRequest
+    {
+        public int PendingRequestCount { get; set; }
+        public string FrontEnd { get; set; }
+        public string Worker { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
